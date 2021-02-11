c2bbecce800db84feff05d26b464cda242ef6dbc
Shivam Agarwal <44562091+shivamag00@users.noreply.github.com>
2021-02-11 13:50:40 +0000

    Added API for fetching all articles by tag

--- app/controllers/leetcode_articles_controller.rb
+++ app/controllers/leetcode_articles_controller.rb
@@ -12,4 +12,11 @@ class LeetcodeArticlesController < Devise::RegistrationsController
     @tags = Tag.all.as_json(only: [:id, :name])
     render :json => @tags
   end
+
+  def get_by_tag
+    sql = "SELECT * from public.articles WHERE cached_tag_list LIKE '%"+params[:tag]+"%';"
+    @tag_articles = Article.find_by_sql(sql).as_json(only: [:id, :title, :slug, :description, :processed_html, :tag_list])
+    msg = { :status => "ok", :message => "Success!", :html => "<b>...</b>" }
+    render :json => @tag_articles
+  end
 end
