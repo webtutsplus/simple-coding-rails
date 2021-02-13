b9ee80c1f9129b9a1ba0d2e51786222c2cf89891
Shivam Agarwal <44562091+shivamag00@users.noreply.github.com>
2021-02-13 02:32:09 +0000

    Added SSL

--- config/puma.rb
+++ config/puma.rb
@@ -15,8 +15,8 @@ port        ENV.fetch("PORT", 3000)
 #
 environment ENV.fetch("RAILS_ENV") { "development" }
 
-localhost_key = "#{File.join('/etc/letsencrypt/live', 'simplecoding.dev', 'privkey.pem')}"
-localhost_crt = "#{File.join('/etc/letsencrypt/live', 'simplecoding.dev', 'cert.pem')}"
+localhost_key = '/etc/letsencrypt/live/simplecoding.dev/privkey.pem'
+localhost_crt = '/etc/letsencrypt/live/simplecoding.dev/cert.pem'
 
 ssl_bind '0.0.0.0', 3000, {
   key: localhost_key,
