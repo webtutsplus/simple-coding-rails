591a4a66131ecadfc31343643e7c872d4ac1dcb1
nmadhab <nilmadhab1994@booking.com>
2021-02-20 10:57:13 +0530

    AuthenticationByApiController implemented

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -201,17 +201,4 @@ class ApplicationController < ActionController::Base
 
     "?i=i"
   end
-
-  def authorize_request_from_api
-    header = request.headers['Authorization']
-    header = header.split(' ').last if header
-    begin
-      @decoded = JsonWebToken.decode(header)
-      @current_user = User.find(@decoded[:user_id])
-    rescue ActiveRecord::RecordNotFound => e
-      render json: { errors: e.message }, status: :unauthorized
-    rescue JWT::DecodeError => e
-      render json: { errors: e.message }, status: :unauthorized
-    end
-  end
 end
